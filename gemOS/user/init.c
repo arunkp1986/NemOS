e5f8d03335ae114e78bf284fd7bb7566a6358f93
arunkp <arunkp1986@gmail.com>
2023-01-28 03:36:58 +0000

    init changes

--- gemOS/user/init.c
+++ gemOS/user/init.c
@@ -13,8 +13,10 @@ int main(u64 arg1, u64 arg2, u64 arg3, u64 arg4, u64 arg5)
 {  
    char * ptr1 = (char*)mmap(NULL,4096,PROT_WRITE,MAP_NVM);
    ptr1[0] = 'A';
+   printf("ptr1:%x, value:%c\n",ptr1,ptr1[0]);
    char * ptr2 = (char*)mmap(NULL,4096,PROT_WRITE,0);
    ptr2[0] = 'B';
+   printf("ptr2:%x, value:%c\n",ptr2,ptr2[0]);
    munmap(ptr1, 4096);
    munmap(ptr2, 4096);
    return 0;
